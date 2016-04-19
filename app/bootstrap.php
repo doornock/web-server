97d366b72a696bf563d68d4f5034ba0e3914000a
Martin Sadovy <sodae@email.cz>
2016-04-19 09:07:04 +0200

    remove bin/create-user.php bootstrap: explain enable debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-#$configurator->setDebugMode(TRUE);
+//$configurator->setDebugMode(TRUE); // enable debug mode everywhere
+
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
