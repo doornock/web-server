391bf76991d8678f8112cce6651e5a89c72ffa57
Martin Sadovy <sodae@email.cz>
2016-04-22 20:52:02 +0200

    Add Qrcode generator

--- www/assets/app/js/main.js
+++ www/assets/app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-
+    $('.qrUrlPopOver').each(function () {
+        var createElement = document.createElement('div');
+        new QRCode(createElement, $(this).data('qr-url'));
+        $(this).popover({
+            'content': createElement,
+            'placement': 'left',
+            'html': true
+        });
+    });
 });
